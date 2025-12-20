2f8548fc9407a2fe69003a59f3c97218ff0a5bae
Denis Erokhin <denis.erokhin@opendaq.com>
2025-12-20 18:26:07 +0100

    remove unused method

--- property/src/dict_property_item.cpp
+++ property/src/dict_property_item.cpp
@@ -3,67 +3,6 @@
 #include <QTreeWidgetItem>
 
 // ============================================================================
-// Helper functions to convert BaseObject to/from string
-// ============================================================================
-
-static daq::BaseObjectPtr stringToBaseObject(const QString& str, const daq::BaseObjectPtr& reference)
-{
-    if (!reference.assigned())
-    {
-        // Try to infer type from string
-        bool ok;
-        int intVal = str.toInt(&ok);
-        if (ok)
-            return daq::Integer(intVal);
-
-        double doubleVal = str.toDouble(&ok);
-        if (ok)
-            return daq::Float(doubleVal);
-
-        if (str == "true" || str == "false")
-            return daq::Boolean(str == "true");
-
-        return daq::String(str.toStdString());
-    }
-
-    try
-    {
-        if (reference.asPtrOrNull<daq::IInteger>(true).assigned())
-        {
-            bool ok;
-            int val = str.toInt(&ok);
-            if (!ok)
-                throw std::runtime_error("Invalid integer value");
-            return daq::Integer(val);
-        }
-        else if (reference.asPtrOrNull<daq::IString>(true).assigned())
-        {
-            return daq::String(str.toStdString());
-        }
-        else if (reference.asPtrOrNull<daq::IBoolean>(true).assigned())
-        {
-            if (str != "true" && str != "false")
-                throw std::runtime_error("Invalid boolean value (must be 'true' or 'false')");
-            return daq::Boolean(str == "true");
-        }
-        else if (reference.asPtrOrNull<daq::IFloat>(true).assigned())
-        {
-            bool ok;
-            double val = str.toDouble(&ok);
-            if (!ok)
-                throw std::runtime_error("Invalid float value");
-            return daq::Float(val);
-        }
-    }
-    catch (...)
-    {
-        throw;
-    }
-
-    return daq::String(str.toStdString());
-}
-
-// ============================================================================
 // DictPropertyItem implementation
 // ============================================================================
 
