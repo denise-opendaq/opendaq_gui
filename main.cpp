675e88657bd6e36fbdd3a6100065a6b983b0c9e7
Denis Erokhin <denis.erokhin@opendaq.com>
2026-01-02 12:23:48 +0100

    add manufacturer and serial number

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QDir>
+#include <QSysInfo>
 
 #include "MainWindow.h"
 #include "context/AppContext.h"
@@ -7,16 +8,39 @@
 #include <opendaq/instance_factory.h>
 #include <opendaq/logger_sink_ptr.h>
 #include <opendaq/log_level.h>
+#include <opendaq/device_info_factory.h>
+
+#ifdef Q_OS_WIN
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    // Generate unique device info
+    QString manufacturer = QString("openDAQ-GUI-%1").arg(QSysInfo::machineHostName());
+
+    // Get process ID for unique serial number
+#ifdef Q_OS_WIN
+    qint64 pid = GetCurrentProcessId();
+#else
+    qint64 pid = getpid();
+#endif
+    QString serialNumber = QString::number(pid);
+
+    auto deviceInfo = daq::DeviceInfo("daqmock://client_device", "OpenDAQClient");
+    deviceInfo.setManufacturer(manufacturer.toStdString());
+    deviceInfo.setSerialNumber(serialNumber.toStdString());
 
     // Create openDAQ instance with our custom logger sink from AppContext
     auto builder = daq::InstanceBuilder().setGlobalLogLevel(daq::LogLevel::Info)
                                          .addLoggerSink(AppContext::Instance()->getLoggerSink())
                                          .addLoggerSink(daq::StdOutLoggerSink())
                                          .addDiscoveryServer("mdns")
+                                         .setDefaultRootDeviceInfo(deviceInfo)
                                          .setUsingSchedulerMainLoop(true);
 
     // Add extra module path if defined (for bundle packaging)
