fd4c14f00048f5f27da659f238c85c259c78214b
Denis Erokhin <denis.erokhin@opendaq.com>
2026-05-01 14:46:40 +0200

    try to update for linux and windows

--- cmake/bundle_configuration.cmake
+++ cmake/bundle_configuration.cmake
@@ -15,7 +15,7 @@ if(APPLE)
         MACOSX_BUNDLE_BUNDLE_NAME "openDAQ Qt GUI"
         MACOSX_BUNDLE_BUNDLE_VERSION "${PROJECT_VERSION}"
         MACOSX_BUNDLE_SHORT_VERSION_STRING "${PROJECT_VERSION}"
-        MACOSX_BUNDLE_INFO_PLIST "${CMAKE_CURRENT_LIST_DIR}/MacOSXBundleInfo.plist.in"
+        MACOSX_BUNDLE_INFO_PLIST "${CMAKE_CURRENT_LIST_DIR}/packaging/mac/MacOSXBundleInfo.plist.in"
     )
 
     # Use install RPATH at build time too so CMake never adds the build-tree
@@ -42,7 +42,7 @@ if(APPLE)
     set(BIN_DIR "${CMAKE_BINARY_DIR}/bin")
     set(MACDEPLOYQT "${MACDEPLOYQT_EXECUTABLE}")
     configure_file(
-        "${CMAKE_CURRENT_LIST_DIR}/prepare_app_bundle.cmake.in"
+        "${CMAKE_CURRENT_LIST_DIR}/packaging/mac/prepare_app_bundle.cmake.in"
         "${_BUNDLE_PREP_SCRIPT}"
         @ONLY
     )
@@ -58,4 +58,71 @@ elseif(UNIX)
         BUILD_RPATH "\$ORIGIN/../lib/opendaq-qt-gui:\$ORIGIN/../lib/opendaq-qt-gui/qt6"
         INSTALL_RPATH "\$ORIGIN/../lib/opendaq-qt-gui:\$ORIGIN/../lib/opendaq-qt-gui/qt6"
     )
+
+    # Install rules used by CPack (DEB) on Linux
+    install(TARGETS ${PROJECT_NAME}
+            RUNTIME DESTINATION "bin"
+            COMPONENT app)
+
+    if(TARGET Qt6::qmake)
+        get_target_property(_qmake_executable Qt6::qmake IMPORTED_LOCATION)
+        execute_process(
+            COMMAND "${_qmake_executable}" -query QT_INSTALL_LIBS
+            OUTPUT_VARIABLE _qt_lib_dir
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        execute_process(
+            COMMAND "${_qmake_executable}" -query QT_INSTALL_PLUGINS
+            OUTPUT_VARIABLE _qt_plugins_dir
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+        set(_LINUX_INSTALL_SCRIPT "${CMAKE_BINARY_DIR}/cmake/packaging/linux/install_linux_bundle.cmake")
+        file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/cmake/packaging/linux")
+
+        set(APP_NAME "opendaq-qt-gui")
+        set(PROJECT_BINARY_NAME "opendaq_qt_gui")
+        set(QT_LIB_DIR "${_qt_lib_dir}")
+        set(QT_PLUGINS_DIR "${_qt_plugins_dir}")
+        set(BUILD_BIN_DIR "${CMAKE_BINARY_DIR}/bin")
+
+        configure_file(
+            "${CMAKE_CURRENT_LIST_DIR}/packaging/linux/install_linux_bundle.cmake.in"
+            "${_LINUX_INSTALL_SCRIPT}"
+            @ONLY
+        )
+
+        install(SCRIPT "${_LINUX_INSTALL_SCRIPT}" COMPONENT app)
+    endif()
+endif()
+
+# ── Windows ──────────────────────────────────────────────────────────────────
+if(WIN32)
+    install(TARGETS ${PROJECT_NAME}
+            RUNTIME DESTINATION "bin"
+            COMPONENT app)
+
+    if(TARGET Qt6::qmake)
+        get_target_property(_qmake_executable Qt6::qmake IMPORTED_LOCATION)
+        get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
+        find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
+
+        if(WINDEPLOYQT_EXECUTABLE)
+            set(_WIN_INSTALL_SCRIPT "${CMAKE_BINARY_DIR}/cmake/packaging/windows/install_windows_bundle.cmake")
+            file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/cmake/packaging/windows")
+
+            set(APP_NAME "OpenDAQ Qt GUI")
+            set(PROJECT_BINARY_NAME "opendaq_qt_gui")
+            set(BUILD_BIN_DIR "${CMAKE_BINARY_DIR}/bin")
+            set(WINDEPLOYQT "${WINDEPLOYQT_EXECUTABLE}")
+
+            configure_file(
+                "${CMAKE_CURRENT_LIST_DIR}/packaging/windows/install_windows_bundle.cmake.in"
+                "${_WIN_INSTALL_SCRIPT}"
+                @ONLY
+            )
+
+            install(SCRIPT "${_WIN_INSTALL_SCRIPT}" COMPONENT app)
+        endif()
+    endif()
 endif()
